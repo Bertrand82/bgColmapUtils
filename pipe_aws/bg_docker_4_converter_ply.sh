51f847b617abec7b447e77f8da1460c4d3d567a5
Bertrand82 <bertrand.guiral@gmail.com>
2026-02-24 15:03:53 +0100

    pluri i

--- pipe_aws/bg_docker_4_converter_ply.sh
+++ pipe_aws/bg_docker_4_converter_ply.sh
@@ -7,18 +7,32 @@ echo "========================================"
 echo
 source ./bgInitConfig.sh
 echo "OUTPUT_DIR:  $OUTPUT_DIR"
-echo process only sparse/0 TODO process all
+
+BASE="/data/images_test/output/sparse"
+
+i=0
+while [ -d "${BASE}/${i}" ]; do
+	echo "=== Processing sparse/${i} ==="
+	
 
 docker run --rm \
   -v "${OUTPUT_DIR}:/data" \
   colmap/colmap \
   colmap model_converter \
-    --input_path /data/sparse/0 \
-    --output_path /data/sparse/0/points3D.ply \
+    --input_path /data/sparse/${i} \
+    --output_path /data/sparse/${i}/points3D.ply \
     --output_type PLY
 
-echo
+  
+  i=$((i+1))
+  echo "Terminé: aucun dossier ${BASE}/${i} ."
+done
+
+echo "Fin Conversion en PLY  ${BASE}   (arrêt)."
+
+echo process only sparse/0 TODO process all
+
+
 echo "========================================"
 echo Traitement termine colmap model_converter .PLY
 echo "========================================"
-read -rp "Appuyez sur Entrée pour continuer..."
