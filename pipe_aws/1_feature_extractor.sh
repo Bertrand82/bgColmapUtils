3cb8cc9b12cc6687b3eaa460f02d0284fa1e95a4
Ubuntu <ubuntu@ip-172-31-42-244.eu-north-1.compute.internal>
2026-02-26 12:47:49 +0000

    LOG

--- pipe_aws/1_feature_extractor.sh
+++ pipe_aws/1_feature_extractor.sh
@@ -7,13 +7,24 @@ echo " COLMAP - feature_extractor v bg 02"
 echo "========================================"
 echo
 source ./bgInitConfig.sh
+touch $LOG
 echo "IMAGES_DIR: $IMAGES_DIR"
+echo "$(date '+%Y-%m-%d %H:%M:%S') -   Début du pipeline feature extraction" > $LOG
 NB_IMAGES=$(find "$IMAGES_DIR" -maxdepth 1 -type f | wc -l)
-echo "Image count: $NB_IMAGES"
-echo "OUTPUT_DIR:  $OUTPUT_DIR"
+echo "Image count in file :   $NB_IMAGES">>$LOG
+echo "OUTPUT_DIR:  $OUTPUT_DIR">>$LOG
+echo "DATABASE_NAME : $DATABASE_NAME">>$LOG
+echo "DATABASE_PATH : $DATABASE_PATH">>$LOG
+echo "LOG        : $LOG"
 mkdir -p "$OUTPUT_DIR"
 
 echo "========  feature_extractor ================================"
+touch $LOG
+echo "$(date '+%Y-%m-%d %H:%M:%S') -   Début du pipeline feature extraction" > log.txt
+START_EPOCH=$(date +%s)
+
+
+
 
 docker run --rm \
   --memory="12g" \
@@ -30,3 +41,11 @@ docker run --rm \
     --FeatureExtraction.use_gpu 0 \
     --FeatureExtraction.num_threads 1 \
     --log_level 0
+
+echo "Fin docker">>$LOG
+END_EPOCH=$(date +%s)
+DURATION_SEC=$((END_EPOCH - START_EPOCH))
+DURATION_MIN=$(( (DURATION_SEC + 59) / 60 ))  # arrondi à la minute supérieure
+echo "DURATION_MN = $DURATION_MIN" >> $LOG
+NB_IMAGES_DB=$(sqlite3 "$DATABASE_PATH" "SELECT COUNT(*) FROM images;")
+echo "NB_IMAGES_DB = $NB_IMAGES_DB" >> $LOG
--- pipe_aws/bgInitConfig.sh
+++ pipe_aws/bgInitConfig.sh
@@ -9,8 +9,9 @@ export IMAGES_DIR="${WORK_DIRECTORY}/images"
 export OUTPUT_DIR="${WORK_DIRECTORY}/output"
 #exhaustive_matcher COLMAP_MATCHER, sequential_matcher , spatial_matcher , vocab_tree_matcher , transitive_matcher
 export COLMAP_MATCHER="sequential_matcher"
-
-
+export LOG="$OUTPUT_DIR/log.txt"
+export DATABASE_NAME="database.db"
+export DATABASE_PATH="$OUTPUT_DIR/$DATABASE_NAME"
 echo " OUTPUT_DIR     : $OUTPUT_DIR  "
 echo " IMAGES_DIR     : $IMAGES_DIR "
 echo " COLMAP_MATCHER : $COLMAP_MATCHER"
