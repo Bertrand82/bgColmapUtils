f3ecef4821f072b5a2c83bd288c602814983f43a
Bertrand82 <bertrand.guiral@gmail.com>
2026-03-02 18:52:50 +0100

    s

--- pipe_aws/66_model_merger.sh
+++ pipe_aws/66_model_merger.sh
@@ -14,9 +14,16 @@ mkdir -p "${OUTPUT_DIR}/sparse"
 mkdir -p "${OUTPUT_DIR}/sparse/model"
 export i=0
 export j=1
-MERGED="merged_${i}_${j}"
-export MERGED
-mkdir -p ${OUTPUT_DIR}/sparse/${MERGED}
+export MERGED2="merged2"
+mkdir -p ${OUTPUT_DIR}/sparse/${MERGED2}
+cp -a ${OUTPUT_DIR}/sparse/0/. /${OUTPUT_DIR}/sparse/$MERGED2/
+while [ -d "${BASE}/${j}" ]; do
+echo "=== Processing ${BASE}/${i} ==="
+MERGED_OUT="merged_out__${i}_${j}"
+
+export MERGED_OUT
+mkdir -p ${OUTPUT_DIR}/sparse/${MERGED_OUT}
+
 echo "-------------------->start merging"
 docker run --rm \
   --memory="12g" \
@@ -27,12 +34,12 @@ docker run --rm \
   -v "${OUTPUT_DIR}:/output" \
   colmap/colmap \
   colmap  model_merger \
-    --input_path1 /output/sparse/$i \
+    --input_path1 /output/sparse/$MERGED2 \
     --input_path2 /output/sparse/$j \
-    --output_path /output/sparse/$MERGED \
+    --output_path /output/sparse/$MERGED_OUT \
     --max_reproj_error 64
 
-echo "--------> merge done  $MERGED "
+echo "--------> merge done  $MERGED_OUT "
 echo "---------> start converting"
  docker run --rm \
   --user "$(id -u):$(id -g)" \
@@ -40,17 +47,21 @@ echo "---------> start converting"
   -v "${OUTPUT_DIR}:/output" \
   colmap/colmap \
   colmap model_converter \
-    --input_path /output/sparse/${MERGED} \
-    --output_path /output/sparse/${MERGED} \
+    --input_path /output/sparse/${MERGED_OUT} \
+    --output_path /output/sparse/${MERGED_OUT} \
     --output_type TXT
 
   
-  echo "convert done : dossier  $MERGED">>$LOG
+  echo "convert done : dossier  $MERGED_">>$LOG
   export BASE="$OUTPUT_DIR/sparse"
-  wc -l ${BASE}/${MERGED}/images.txt>>$LOG
-  echo "Terminé: dossier  $MERGED  ">>$LOG
-  wc -l ${BASE}/${MERGED}/images.txt>>$LOG
-    
+  wc -l ${BASE}/${MERGED_OUT}/images.txt>>$LOG
+  echo "Terminé: dossier  $MERGED_OUT  ">>$LOG
+  wc -l ${BASE}/${MERGED_OUT}/images.txt>>$LOG
+  cp -a ${OUTPUT_DIR}/sparse/${MERGED_OUT}/. /${OUTPUT_DIR}/sparse/$MERGED2/
+# rm -rf ${OUTPUT_DIR}/sparse/${MERGED_OUT}
+  i=$((i+1))
+  j=$((j+1))
+ done   
 echo
 echo "========================================"
 echo "Process colmap model_merger done"
