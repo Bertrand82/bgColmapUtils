743d4ccd710a8e631bbacec2d22748f9b9222d65
Ubuntu <ubuntu@ip-172-31-42-244.eu-north-1.compute.internal>
2026-03-03 06:05:16 +0000

    s

--- pipe_aws/bgInitConfig.sh
+++ pipe_aws/bgInitConfig.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Chemin absolu du dossier où se trouve ce fichier (peu importe d'où tu le "source")
-export WORK_DIRECTORY="/data/vol_0"
+export WORK_DIRECTORY="/data/vol_full"
 
 
 # (optionnel) quelques chemins utiles
