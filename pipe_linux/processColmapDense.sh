2ddaaeb3ff106354a561395bc9b93c49ca9eaa3e
Bertrand <bertrand.guiral@gmail.com>
2026-04-15 23:18:22 +0200

    s

--- pipe_linux/processColmapDense.sh
+++ pipe_linux/processColmapDense.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 HOME_COLMAP="$HOME/workspaceCpp/colmap/build/src/colmap/exe"
-COLMAP_EXE_PATH="$HOME/workspaceCpp/colmap/build/src/colmap/exe"
+export COLMAP_EXE_PATH="$HOME/workspaceCpp/colmap/build/src/colmap/exe"
 # You must set $COLMAP_EXE_PATH to 
 # the directory containing the COLMAP executables.
 echo "HOME_COLMAP : $HOME_COLMAP" 
@@ -34,10 +34,10 @@ $HOME_COLMAP/colmap patch_match_stereo \
   --output_path $BG_WORK/dense/fused.ply
   
  echo "----------- bg --- poisson_mesher ----------------------------------------------"  
- 
-$HOME_COLMAP/colmap poisson_mesher \
-  --input_path $BG_WORK/dense \
-  --output_path $BG_WORK/dense/mesh_poisson.ply
+ $HOME_COLMAP/colmap poisson_mesher \
+  --input_path "$BG_WORK/dense/fused.ply" \
+  --output_path "$BG_WORK/dense/mesh_poisson.ply"
+
 echo "----------- bg --- patch_match_stereo ----------------------------------------------"  
   
 $HOME_COLMAP/colmap patch_match_stereo \
@@ -50,7 +50,7 @@ $HOME_COLMAP/colmap stereo_fusion \
   --workspace_path $BG_WORK/dense \
   --workspace_format COLMAP \
   --input_type geometric \
-  --output_path $BG_WORK/dense/fused.ply
+  --output_path $BG_WORK/dense/fused2.ply
  
  echo "----------- bg --- delaunay_mesher ----------------------------------------------"  
 
@@ -59,6 +59,25 @@ $HOME_COLMAP/colmap stereo_fusion \
  echo "----------- bg --- poisson_mesher ----------------------------------------------"  
 
  $HOME_COLMAP/colmap poisson_mesher \
-  --input_path $BG_WORK/dense \
-  --output_path $BG_WORK/dense/mesh_poisson.ply
+  --input_path $BG_WORK/dense/fused2.ply \
+  --output_path $BG_WORK/dense/mesh_poisson2.ply
+  echo " done -----------------------------------------------------"
+
+  directoryCurrent=$(pwd)
+  echo "directoryCurrent = $directoryCurrent"
+  cleanup() {
+  # Toujours revenir au répertoire de départ
+  cd "$directoryCurrent" || true
+}
+trap cleanup EXIT INT TERM
+  cd $BG_WORK/dense
+  pwd
+  echo " run-colmap-geometric.sh -----------------------------------------------------"
+  bash $BG_WORK/dense/run-colmap-geometric.sh 
+  echo " run-colmap-photometric.sh -----------------------------------------------------"
+
+  bash $BG_WORK/dense/run-colmap-photometric.sh
+  echo " done -------------------------------------------"
+  cd $directoryCurrent
+  pwd
   
