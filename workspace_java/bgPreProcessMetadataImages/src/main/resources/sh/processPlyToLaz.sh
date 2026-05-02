566f4389d84113e7b3beb1b0f247cdea4d192189
Bertrand <bertrand.guiral@gmail.com>
2026-05-02 13:49:18 +0200

    s

--- workspace_java/bgPreProcessMetadataImages/src/main/resources/sh/processPlyToLaz.sh
+++ workspace_java/bgPreProcessMetadataImages/src/main/resources/sh/processPlyToLaz.sh
@@ -3,14 +3,19 @@ set -euo pipefail
 
 PDAL=~/workspaceCpp/PDAL/build/bin/pdal
 IN="merged.ply"
-OUT=""
-echo "bg=data PDAL=$PDAL"
+OUT_LAZ="fused.laz"
+OUT_LAS="fused.las"
+echo "bg=data PDAL=$PDAL step=translate_to_laz IN=$IN OUT_LAZ=$OUT_LAZ"
 echo "bg=data IN=$IN"
 #pdal translate fused.ply fused.laz
 # ~/workspaceCpp/PDAL/build/bin/pdal translate merged.ply fused.laz
-"$PDAL translate $IN $OUT" 
-
+"$PDAL translate $IN $OUT_LAZ" 
+# ~/workspaceCpp/PDAL/build/bin/pdal translate fused.laz fused.las
+# ~/workspaceCpp/PDAL/build/bin/pdal translate merged.ply fused.las
+echo "bg=data PDAL=$PDAL step=translate_to_las  OUT_LAS=$OUT_LAS"
+"$PDAL translate $IN IN=$IN $OUT_LAS" 
 #Vérifier rapidement : pdal info fused.laz | head
-# ~/workspaceCpp/PDAL/build/bin/pdal info fused.laz
+# ~/workspaceCpp/PDAL/build/bin/pdal info merged.ply fused.laz
 "$PDAL info $OUT | head" 
+
 
