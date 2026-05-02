ef0101931cd849b8fee1126bcf57dd890e3190cc
Bertrand <bertrand.guiral@gmail.com>
2026-05-02 18:30:23 +0200

    s

--- workspace_java/bgPreProcessMetadataImages/src/main/resources/sh/processPlyToLaz.sh
+++ workspace_java/bgPreProcessMetadataImages/src/main/resources/sh/processPlyToLaz.sh
@@ -6,15 +6,16 @@ IN="merged.ply"
 OUT_LAZ="fused.laz"
 OUT_LAS="fused.las"
 
-echo "bg=data PDAL=$PDAL step=translate_to_laz IN=$IN OUT_LAZ=$OUT_LAZ"
+echo "bg=data PDAL=$PDAL step=translate_to_laz IN=$IN OUT_LAZ=$OUT_LAZ date=$(date -Is)"
 "$PDAL" translate "$IN" "$OUT_LAZ"
 
-echo "bg=data PDAL=$PDAL step=translate_to_las OUT_LAS=$OUT_LAS"
+echo "bg=data PDAL=$PDAL step=translate_to_las IN=$IN OUT_LAS=$OUT_LAS date=$(date -Is)"
 "$PDAL" translate "$IN" "$OUT_LAS"
 
-echo "bg=data step=info_laz file=$OUT_LAZ"
-"$PDAL" info "$OUT_LAZ" | head
-
 py3dtiles="/home/bertrand/venv/py3dtiles/bin/py3dtiles"
-echo "bg=data py3dtiles=$py3dtiles"
-"$py3dtiles" convert "$OUT_LAS" --out tiles_fused --overwrite --spec-version 1.0
+echo "bg=data step=py3dtiles state="start" file=tiles_fused py3dtiles=$py3dtiles date=$(date -Is)"
+
+"$py3dtiles" convert "$OUT_LAS" --out tiles_fused --overwrite --spec-version 1.0 --color_scale 256
+echo "bg=data step=py3dtiles state="done" file=tiles_fused date=$(date -Is)"
+
+echo "bg=data  step=fin date=$(date -Is)"
